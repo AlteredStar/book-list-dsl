b093ef064dc3abf95c8b913dca4a2036890264ad
Kevin Nguyen <kevindwin@hotmail.com>
2025-06-17 09:49:35 -0500

    added second line test case for scala dsl

--- scala/src/main/scala/BookListDSL.scala
+++ scala/src/main/scala/BookListDSL.scala
@@ -6,4 +6,12 @@ object add extends Dynamic {
   def book =
     bookDetails.append(s"Add Book:")
     bookDetails.result
+
+  def book(bookBlock : add.type => add.type) =
+    bookBlock(this)
+    bookDetails.result
+
+  infix def typed(bookType : String) =
+    bookDetails.append(s" ${bookType}")
+    this
 }
--- scala/src/test/scala/BookListTest.scala
+++ scala/src/test/scala/BookListTest.scala
@@ -1,9 +1,17 @@
 import scala.language.postfixOps
 
 class BookListTest extends munit.FunSuite {
-  test("first line of book lsit DSL") {
+  test("first line of book list DSL") {
     val result = add book
 
     assertEquals("Add Book:", result)
+  }
+
+  test("second line of book list DSL") {
+    val result = add book {
+      _ typed "Web Novel"
+    }
+
+    assertEquals("Add Book: Web Novel", result)
   }
 }
